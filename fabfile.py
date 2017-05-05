fe0c782b7ef5681743e5c78656fb7f5fc1698611
rajit88 <pavan.itsyou@gmail.com>
2017-05-05 14:23:23 +0530

    test1

--- fabfile.py
+++ fabfile.py
@@ -3,5 +3,5 @@ from fabric.api import local
 def test1():
 	local("cd /home/devops/fabric/fabrictest && touch file1 file2 file3")
 	local("cd /home/devops/fabric/fabrictest && git add . && git commit -m test1")
-	local("cd /home/devops/fabric/fabrictest && git push orgin master")
+	local("cd /home/devops/fabric/fabrictest && git push origin master")
 
